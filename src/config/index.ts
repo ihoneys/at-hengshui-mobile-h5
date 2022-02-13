b95a4cbadfff95e2aee17d247ae60a22c42a82cf
热爱一千万的offer <thefrontliu@163.com>
2022-02-13 18:31:09 +0800

    Update index.ts
    modify of the config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 export const sm4Config = {
-  key: '4fbxytabWE266492',
+  key: '4fbxytabWExxxx',
   // optional; can be 'cbc' or 'ecb'
   mode: 'ecb', // default
   // optional; when use cbc mode, it's necessary
-  iv: 'UISwD9fW6cFh9SNS', // default is null
+  iv: 'UISwD9fxxxx', // default is null
   // optional: this is the cipher data's type; Can be 'base64' or 'text'
   cipherType: 'text', // default is base64
 }
